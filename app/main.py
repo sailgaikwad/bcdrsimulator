df4df574e36e1360638cc6d9b02deee204365918
Sail <sailgaikwad108@gmail.com>
2026-08-17 14:39:52 +0530

    Fix ModuleNotFoundError on Streamlit Cloud

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # Configure the main Streamlit page
 st.set_page_config(
     page_title="BC/DR Simulator",
